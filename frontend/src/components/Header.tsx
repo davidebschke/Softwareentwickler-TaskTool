2d2f22ab29710b086986b86a960bfda4316c3487
davidebschke <Davidebschke8@gmail.com>
2022-08-16 17:10:14 +0200

    fixes

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -18,21 +18,17 @@ export default function Header() {
     const pages = ['Products', 'Pricing', 'Blog'];
     const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
-
         const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
         const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
-
         const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
             setAnchorElNav(event.currentTarget);
         };
         const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
             setAnchorElUser(event.currentTarget);
         };
-
         const handleCloseNavMenu = () => {
             setAnchorElNav(null);
         };
-
         const handleCloseUserMenu = () => {
             setAnchorElUser(null);
         };
