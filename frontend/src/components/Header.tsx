25063638718df44e4efbdd42acfe5ab06fe7d50b
davidebschke <Davidebschke8@gmail.com>
2022-08-16 15:38:35 +0200

    Insert Logo

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,17 @@
+
+export default function Header (){
+
+return(
+    <header>
+
+        <div>
+            <img src={"../exampleLogo.jpg"} alt={"logo"}/> <h1> Hallo Welt </h1>
+        </div>
+
+    </header>
+
+
+)
+
+
+}
