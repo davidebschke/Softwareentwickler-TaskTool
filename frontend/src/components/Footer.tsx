ac6aefd1a80b637e50d4495f54c5b195af441ac8
davidebschke <Davidebschke8@gmail.com>
2022-08-17 11:20:59 +0200

    Footer.tsx include buttons (no function)

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,31 +1,23 @@
 import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
+import AdbRoundedIcon from '@mui/icons-material/AdbRounded';
+import CottageRoundedIcon from '@mui/icons-material/CottageRounded';
 import "./footer.css"
+
 export default function Footer(){
 
-
     return(
-
-
-
         <footer>
-
             <div className="footer-content">
                 <p>Source Code by David Ebschke</p>
                 <ul className="socials">
-                    <li><a href="https://facebook.com"><i className="fa fa-facebook"><FacebookRoundedIcon/> </i></a></li>
-                    <li><a href="#"><i className="fa fa-twitter"></i></a></li>
-                    <li><a href="#"><i className="fa fa-google-plus"></i></a></li>
-                    <li><a href="#"><i className="fa fa-youtube"></i></a></li>
-                    <li><a href="#"><i className="fa fa-linkedin-square"></i></a></li>
+                    <li><a href="https://google.de"><i className="HomeBottom"></i><CottageRoundedIcon/></a></li>
+                    <li><a href="https://facebook.com"><i className="Facebook"> <FacebookRoundedIcon/></i></a></li>
+                    <li><a href="https://google.de"><i className="BugReport"><AdbRoundedIcon/></i></a></li>
                 </ul>
                 <div className="footer-bottom">
                     <p>copyright &copy;2022 <a href="https://github.com/davidebschke">David Ebschke</a>  </p>
                 </div>
             </div>
-
         </footer>
     )
-
-
-
 }
