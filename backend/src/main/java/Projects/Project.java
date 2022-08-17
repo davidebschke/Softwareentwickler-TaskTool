eade74d1fd52398854952a055450a3e4509be8c9
davidebschke <Davidebschke8@gmail.com>
2022-08-17 11:40:14 +0200

    create Project Class with lombok builder and Validation

---
+++ backend/src/main/java/Projects/Project.java
@@ -0,0 +1,19 @@
+package Projects;
+
+import lombok.Builder;
+
+import javax.validation.constraints.NotNull;
+
+@Builder
+public class Project
+{
+
+@NotNull
+    Integer ProjektNummer;
+
+@NotNull
+    String ProjektName;
+
+@NotNull
+    String Status;
+}
