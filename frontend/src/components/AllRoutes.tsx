2fbf40b69960a947565ae6e06e2491d7d58cca97
davidebschke <Davidebschke8@gmail.com>
2022-08-19 13:34:45 +0200

    heroku fix

--- frontend/src/components/AllRoutes.tsx
+++ frontend/src/components/AllRoutes.tsx
@@ -2,13 +2,6 @@ import {Route, Routes} from "react-router-dom";
 import useProjects from "./useProjects";
 import Home from "./Home";
 import ProjectsShow from "./ProjectsShow";
-import {Project} from "./Project";
-
-type ProjectProps = {
-
-    projects: Project[]
-
-}
 
 export default function AllRoutes() {
 
