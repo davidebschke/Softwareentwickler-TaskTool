06f262d4bdfc842daa9ef4569e074fce93036e46
davidebschke <Davidebschke8@gmail.com>
2022-08-22 09:53:48 +0200

    Links zu Facebook und der HerokuApp als Blank definiert und mit noreferrer ausgestattet

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -9,11 +9,11 @@ export default function Footer(){
             <div className="footer-content">
                 <p>Source Code by David Ebschke</p>
                 <ul className="socials">
-                    <li><a href="https://capstone-sett.herokuapp.com/#/home"><i className="HomeBottom"><CottageRoundedIcon/></i></a></li>
-                    <li><a href="https://www.facebook.com/profile.php?id=100080679306056"><i className="Facebook"> <FacebookRoundedIcon/></i></a></li>
+                    <li><a href="https://capstone-sett.herokuapp.com/#/home" target="_blank" rel={"noreferrer"}><i className="HomeBottom"><CottageRoundedIcon/></i></a></li>
+                    <li><a href="https://www.facebook.com/profile.php?id=100080679306056" target="_blank" rel={"noreferrer"}><i className="Facebook"> <FacebookRoundedIcon/></i></a></li>
                 </ul>
                 <div className="footer-bottom">
-                    <p>copyright &copy;2022 <a href="https://github.com/davidebschke">David Ebschke</a>  </p>
+                    <p>copyright &copy;2022 <a href="https://github.com/davidebschke" target="_blank" rel={"noreferrer"}>David Ebschke</a>  </p>
                 </div>
             </div>
         </footer>
