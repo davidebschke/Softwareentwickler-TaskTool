a2d541678ae11dd4a1b96df8f689e9809798c500
davidebschke <Davidebschke8@gmail.com>
2022-08-22 09:46:40 +0200

    Home Seite Verlinkt ( Heroku )

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -10,7 +10,7 @@ export default function Footer(){
             <div className="footer-content">
                 <p>Source Code by David Ebschke</p>
                 <ul className="socials">
-                    <li><a href="https://google.de"><i className="HomeBottom"><CottageRoundedIcon/></i></a></li>
+                    <li><a href="https://capstone-sett.herokuapp.com/#/home"><i className="HomeBottom"><CottageRoundedIcon/></i></a></li>
                     <li><a href="https://www.facebook.com/profile.php?id=100080679306056"><i className="Facebook"> <FacebookRoundedIcon/></i></a></li>
                     <li><a href="https://google.de"><i className="BugReport"><AdbRoundedIcon/></i></a></li>
                 </ul>
