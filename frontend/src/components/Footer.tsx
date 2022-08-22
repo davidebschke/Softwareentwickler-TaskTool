26926e3557dccb1472bb4f7d9bfaf622499284ca
davidebschke <Davidebschke8@gmail.com>
2022-08-22 09:47:20 +0200

    Button BugReport entfernt

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,5 +1,4 @@
 import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
-import AdbRoundedIcon from '@mui/icons-material/AdbRounded';
 import CottageRoundedIcon from '@mui/icons-material/CottageRounded';
 import "./footer.css"
 
@@ -12,7 +11,6 @@ export default function Footer(){
                 <ul className="socials">
                     <li><a href="https://capstone-sett.herokuapp.com/#/home"><i className="HomeBottom"><CottageRoundedIcon/></i></a></li>
                     <li><a href="https://www.facebook.com/profile.php?id=100080679306056"><i className="Facebook"> <FacebookRoundedIcon/></i></a></li>
-                    <li><a href="https://google.de"><i className="BugReport"><AdbRoundedIcon/></i></a></li>
                 </ul>
                 <div className="footer-bottom">
                     <p>copyright &copy;2022 <a href="https://github.com/davidebschke">David Ebschke</a>  </p>
