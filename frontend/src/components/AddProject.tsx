732688dbef72b05b8ddaa127bfefd63d16c733cd
davidebschke <Davidebschke8@gmail.com>
2022-08-22 15:14:33 +0200

    add Dropdown feld für Status, create a enum with DisplayName, Create Enum_Status

--- frontend/src/components/AddProject.tsx
+++ frontend/src/components/AddProject.tsx
@@ -23,7 +23,7 @@ export default function AddProject(props: addProjectProps) {
         setProjectName(event.target.value)
     }
 
-    function onStatusChange(event: ChangeEvent<HTMLInputElement>) {
+    function onStatusChange(event: ChangeEvent<HTMLSelectElement>) {
         setStatus(event.target.value)
     }
 
@@ -37,9 +37,11 @@ export default function AddProject(props: addProjectProps) {
             console.error("Projectnumber must not be empty")
         } else if (!projectName) {
             console.error("ProjectName must not be empty")
-        } else if (!status) {
+        }
+        else if (!status) {
             console.error("Status must not be empty")
-        } else if (!projectMember) {
+        }
+         else if (!projectMember) {
             console.error("Projectmember must not be empty")
         } else {
             props.addProject({projectNumber, projectName, status, projectMember})
@@ -68,9 +70,18 @@ export default function AddProject(props: addProjectProps) {
                 <tr>
                     <td><input type={"number"} value={projectNumber} onChange={onProjectNumberChange}/></td>
                     <td><input type={"text"} value={projectName} onChange={onProjectNameChange}/></td>
-                    <td><input type={"text"} value={status} onChange={onStatusChange}/></td>
+                    <td>
+                        <select id="status" name="status" onChange={onStatusChange}>
+                            <option value="">Bitte Auswählen</option>
+                            <option value="Wait" >Wait</option>
+                            <option value="In_Progress">In Progress</option>
+                            <option value="Done">Done</option>
+                        </select>
+                    </td>
                     <td><input type={"text"} value={projectMember} onChange={onProjectMemberChange}/></td>
-                    <td className={"tableButton"}><button type={"submit"}> Speichern</button></td>
+                    <td className={"tableButton"}>
+                        <button type={"submit"}> Speichern</button>
+                    </td>
                     <td className={"tableButton"}></td>
 
                 </tr>
