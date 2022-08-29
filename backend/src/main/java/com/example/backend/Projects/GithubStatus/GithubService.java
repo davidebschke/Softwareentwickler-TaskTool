72a7c6459b5cd2cc49ffb69aca7bf96cac8a1347
davidebschke <Davidebschke8@gmail.com>
2022-08-29 09:34:45 +0200

    githubServiceTest include

--- backend/src/main/java/com/example/backend/Projects/GithubStatus/GithubService.java
+++ backend/src/main/java/com/example/backend/Projects/GithubStatus/GithubService.java
@@ -11,12 +11,16 @@ import java.util.Objects;
 @AllArgsConstructor
 public class GithubService {
 
+
+
     public List<Issue> getAllIssues() {
 
-        WebClient webClient = WebClient.create();
+        WebClient webClient= WebClient.create();
 
-        return Objects.requireNonNull(webClient.get().uri("https://api.github.com/repos/davidebschke/Softwareentwickler-TaskTool/issues?state=all&per_page=100")
+         return Objects.requireNonNull(webClient.get().uri("https://api.github.com/repos/davidebschke/Softwareentwickler-TaskTool/issues?state=all&per_page=100")
                 .retrieve()
                 .toEntityList(Issue.class).block()).getBody();
+
     }
+
 }
