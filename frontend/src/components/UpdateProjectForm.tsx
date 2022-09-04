d7db686e77e12cdfb449a62149e9c19740acf8a6
davidebschke <Davidebschke8@gmail.com>
2022-09-04 12:13:53 +0200

    code smell fixes frontend

--- frontend/src/components/UpdateProjectForm.tsx
+++ frontend/src/components/UpdateProjectForm.tsx
@@ -36,7 +36,6 @@ export default function UpdateProjectForm(props: UpdateProjectProps) {
                     return project;
 
 
-                } else {
                 }
             })
         }
