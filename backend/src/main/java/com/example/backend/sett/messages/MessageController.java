d7815dde3a33543fc147dec4595181ae3409b83c
davidebschke <Davidebschke8@gmail.com>
2022-09-04 10:00:00 +0200

    create MessageController

---
+++ backend/src/main/java/com/example/backend/sett/messages/MessageController.java
@@ -0,0 +1,10 @@
+package com.example.backend.sett.messages;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/stt/messages")
+public class MessageController {
+}
