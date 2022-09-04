e0fcf4d9602829579a14ff7b35ce149d36cbb795
davidebschke <Davidebschke8@gmail.com>
2022-09-04 11:18:15 +0200

    create MessageRepo,MessageService with getMessages Method

---
+++ backend/src/main/java/com/example/backend/sett/messages/MessageService.java
@@ -0,0 +1,22 @@
+package com.example.backend.sett.messages;
+
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class MessageService {
+
+    MessageRepo messageRepo;
+
+    public List<Onemessage> getMessages() {
+        return messageRepo.findAll();
+    }
+
+    public String getRandomId() {
+        return UUID.randomUUID().toString();
+    }
+
+}
