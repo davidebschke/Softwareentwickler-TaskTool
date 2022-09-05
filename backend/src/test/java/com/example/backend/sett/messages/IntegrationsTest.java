c39b8cd16c18804e349b285ec59c5c9fd7b8cd43
davidebschke <Davidebschke8@gmail.com>
2022-09-05 16:22:19 +0200

    IntegrationsTest für deleteMessage

--- backend/src/test/java/com/example/backend/sett/messages/IntegrationsTest.java
+++ backend/src/test/java/com/example/backend/sett/messages/IntegrationsTest.java
@@ -1,22 +1,13 @@
 package com.example.backend.sett.messages;
 
-import com.example.backend.sett.projects.Projectrepo;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.io.DataInput;
-
-import static org.mockito.Mockito.*;
-import static org.springframework.http.RequestEntity.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -27,6 +18,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class IntegrationsTest {
 
     @Autowired
+    MessageRepo messageRepo;
+    @Autowired
     ObjectMapper objectMapper;
     @Autowired
     MockMvc mockMvc;
@@ -34,5 +27,25 @@ public class IntegrationsTest {
     @Test
     void testGetMessages() throws Exception {
         mockMvc.perform(get("/stt/messages")).andExpect(status().is(200));
+    }
+
+    @DirtiesContext
+    @Test
+    void deleteMessage() throws Exception {
+
+        OneMessage TestObject = new OneMessage("2", "22", "David", "Alf", "Webshop",
+                "2000-01-01", "Hallo Welt Lorum Ipsum is out", "Your Message");
+
+        messageRepo.save(TestObject);
+        String id = TestObject.id;
+
+        mockMvc.perform(delete("/stt/messages/" + id))
+                .andExpect(status().is(204));
+
+        mockMvc.perform(get("/stt/messages"))
+                .andExpect(status().is(200))
+                .andExpect(content().json("""
+                        []
+                        """));
     }
 }
