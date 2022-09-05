705e9ade39a9baf116b2cab0969842dfbf25cde8
davidebschke <Davidebschke8@gmail.com>
2022-09-05 16:36:49 +0200

    IntegrationsTest für deleteMessage v4

--- backend/src/test/java/com/example/backend/sett/messages/IntegrationsTest.java
+++ backend/src/test/java/com/example/backend/sett/messages/IntegrationsTest.java
@@ -1,5 +1,6 @@
 package com.example.backend.sett.messages;
 
+import com.example.backend.sett.githubstatus.OneIssue;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -33,15 +35,18 @@ public class IntegrationsTest {
     @Test
     void deleteMessage() throws Exception {
 
+        Boolean testBoolean = null;
+
         OneMessage TestObject = new OneMessage("2", "22", "David", "Alf", "Webshop",
                 "2000-01-01", "Hallo Welt Lorum Ipsum is out", "Your Message");
 
         messageRepo.save(TestObject);
         String id = TestObject.id;
-
+        
         if (messageRepo.existsById(TestObject.id)) {
             mockMvc.perform(delete("/stt/messages/" + id))
                     .andExpect(status().is(204));
+            testBoolean = true;
         }
 
         mockMvc.perform(get("/stt/messages"))
@@ -49,5 +54,6 @@ public class IntegrationsTest {
                 .andExpect(content().json("""
                         []
                         """));
+        assertThat(testBoolean).isEqualTo(true);
     }
 }
