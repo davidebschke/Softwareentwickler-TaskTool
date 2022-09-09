68bc6f27c0323796fa00cd36817c39a71bb2c0b8
davidebschke <Davidebschke8@gmail.com>
2022-09-09 17:16:19 +0200

    HomeSite Text edit

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -7,48 +7,34 @@ export default function Home() {
         <>
             <Box
                 sx={{
-                    width: 300,
-                    height: 300,
-                    backgroundColor: 'primary.dark',
-                    borderRadius: '50%',
-                    textAlign: 'center',
-                    textTransform: 'uppercase',
-                    marginLeft:3,
-                    marginTop:3,
-                    marginBottom:3,
-                    marginRight:3,
-                    borderStyle:'solid',
+                    width: 1000,
+                    height: 600,
+                    backgroundColor: 'grey',
+                    borderRadius: '3em',
+                    textAlign: 'left',
+                    marginLeft: '300px',
+                    marginTop: 3,
+                    marginBottom: 3,
+                    marginRight: 3,
+                    borderStyle: 'solid',
 
-                }}
-            >
 
+                }}>
                 <div className={'homeCircle'}>
-                    Hier ist der Inhalt der Box
+                    <h1>Capstone Projekt : Softwareentwickler-TaskTool</h1>
+
+                    <h3>Das Programm dient der Verwaltung von Projekten in der Softwareentwicklung. Bei Bedarf ist es
+                        auch für andere Bereiche geeignet.</h3>
+
+                    <h4><p>Folgende Features sind schon implementiert:</p>
+                        <p>
+                            <li> Import von Github Projekten mit Datagrid Attributen</li>
+                            <li> Anzeigen, Einfügen, Editieren und Löschen von Projekten</li>
+                            <li> Anzeigen, Einfügen und Löschen von Nachrichten</li>
+                        </p>
+                    </h4>
                 </div>
             </Box>
-
-        <Box
-            sx={{
-                width: 300,
-                height: 300,
-                backgroundColor: 'primary.dark',
-                borderRadius: '50%',
-                textAlign: 'center',
-                textTransform: 'uppercase',
-                marginLeft:3,
-                marginTop:3,
-                marginBottom:3,
-                marginRight:3,
-                borderStyle:'solid',
-
-            }}
-        >
-
-            <div className={'homeCircle'}>
-                Hier ist der Inhalt der Box
-            </div>
-        </Box>
-
     </>
     )
 }
