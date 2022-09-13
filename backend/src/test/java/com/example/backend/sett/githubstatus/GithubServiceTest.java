df92fc9043bd26346e2706e2d2aacccae156f8c9
davidebschke <Davidebschke8@gmail.com>
2022-09-13 11:14:31 +0200

    codeSmell fix

--- backend/src/test/java/com/example/backend/sett/githubstatus/GithubServiceTest.java
+++ backend/src/test/java/com/example/backend/sett/githubstatus/GithubServiceTest.java
@@ -138,7 +138,6 @@ class GithubServiceTest {
     @DirtiesContext
     void getListofIssuesTest() {
 
-
        List<OneIssue> response = githubService.getIssues();
 
         assertThat(response).hasOnlyElementsOfType(OneIssue.class);
